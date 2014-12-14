b9823d039ff01b2d6d30bd00de24f13d377f858f
Divyansh <divyanshaa12@tamu.edu>
2014-12-14 00:27:31 -0600

    added conv encoder function

---
+++ ConvEnc.m
@@ -0,0 +1,39 @@
+function [c1,c2,tail] = ConvEnc(m)
+
+%%
+%Encoding
+
+%Encoder:
+%	   ------------------------------------------------------- c1
+%	   |
+%	   |        --------+----------+----------+----------+---- c2
+%	   |        |       |          |          |          |
+%	   | m_tmp  |       |          |          |          |
+%	m-----+-------| D |----| D^2 |----| D^3 |----| D^4 |--
+%	      |______________________________________________|    
+%
+%%
+
+for i=1:length(m)
+    c1(i) = m(i); % Since the first one is just the message bit.
+    
+    if i==1
+        in_mem = [0 0 0 0]; %initializing conv encoder's SR's to zero.
+    end
+    
+    m_tmp = mod(in_mem(4) + m(i),2);
+    c2(i) = mod(mod(mod(m_tmp + in_mem(1),2) + mod(in_mem(2) + in_mem(3),2),2) + in_mem(4),2);
+    
+    in_mem = [m_tmp in_mem(1:3)]; % update your encoder's memory
+    
+%BPSK Modulation Ec=1 
+
+    if c1(i)==0
+        c1(i)=-1;
+    end
+    if c2(i)==0
+        c2(i)=-1;
+    end
+end
+
+tail = fliplr(in_mem);
--- main.m
+++ main.m
@@ -2,27 +2,12 @@ clear; clc; close all;
 
 m=[1 0 1 1 1 0 0 0 0 1 1 0 0 1 0 0 0 0 1 0 0 0 0 0];
 
-%%
 %Encoding
+[c1,c2,tail] = ConvEnc(m);
 
-%Encoder:
-%	   ------------------------------------------------------- c1
-%	   |
-%	   |        --------+----------+----------+----------+---- c2
-%	   |        |       |          |          |          |
-%	   | m_tmp  |       |          |          |          |
-%	m-----+-------| D |----| D^2 |----| D^3 |----| D^4 |--
-%	      |______________________________________________|    
+%Noise SD calculation
+R=0.5*(53/63)*(6/5);
+snr=1;
+sd= (2*(10^(snr/10)))^-0.5;
+noise=normrnd(0,sd);
 
-for i=1:length(m)
-    c1(i) = m(i); % Since the first one is just the message bit.
-    
-    if i==1
-        in_mem = [0 0 0 0]; %initializing conv encoder's SR's to zero.
-    end
-    
-    m_tmp = mod(in_mem(4) + m(i),2);
-    c2(i) = mod(mod(mod(m_tmp + in_mem(1),2) + mod(in_mem(2) + in_mem(3),2),2) + in_mem(4),2);
-    
-    in_mem = [m_tmp in_mem(1:3)]; % update your encoder's memory
-end
